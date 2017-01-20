3c214000c2d6e5123fb468ae00bdca3d8ec3a5ad
Slimmmo <Slimmmo@users.noreply.github.com>
2017-01-21 05:34:54 +1100

    Create PGMSuite.js

---
+++ PGMSuite.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         PGMSuite
+// @author       https://github.com/Slimmmo
+// @namespace    PGMSuite
+// @version      1
+// @match        https://*pogomap.com/*
+// @grant        none
+// ==/UserScript==
+
+function indexOfPokemons(pokemon, pokemons) {
+    if ((false || !isPokemonChecked(pokemon.id)) &&
+       (pokemon.attack + pokemon.defence + pokemon.stamina) / 0.45 < 82) {
+        return 0;
+    } else {
+        for (var i = 0; i < pokemons.length; ++i) {
+            var currentPokemon = pokemons[i];
+            if (pokemon.isEqual(currentPokemon)) {
+                return i;
+            }
+        }
+    }
+    postDiscord(pokemon);
+    return -1;
+}
+
+function postDiscord(p) {
+    var webhookURL = ' INSERT DISCORD WEBHOOK HERE ';
+    var date = new Date(p.despawn * 1000);
+    var seconds = "0" + date.getSeconds();
+    var dateString = date.getHours() + ':' + ("0" + date.getMinutes()).substr(-2) + ':' + ("0" + date.getSeconds()).substr(-2);
+    var text = getPokemonName(p) + ' ' + Math.floor((p.attack + p.defence + p.stamina) / 0.45) +
+        '% with ' + getMoveName(p.move1) + ', ' + getMoveName(p.move2) + ' until ' + dateString +
+        ' at http://maps.google.com/maps?q=' + p.center.lat + ',' + p.center.lng + '&zoom=14';
+    $.ajax({
+        data: 'content=' + text,
+        dataType: 'json',
+        processData: false,
+        type: 'POST',
+        url: webhookURL
+    });
+}
+
+addJS_Node (indexOfPokemons);
+addJS_Node (postDiscord);
+
+function addJS_Node (text, s_URL, funcToRun, runOnLoad) {
+    var D                                   = document;
+    var scriptNode                          = D.createElement ('script');
+    if (runOnLoad) {
+        scriptNode.addEventListener ("load", runOnLoad, false);
+    }
+    scriptNode.type                         = "text/javascript";
+    if (text)       scriptNode.textContent  = text;
+    if (s_URL)      scriptNode.src          = s_URL;
+    if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
+
+    var targ = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
+    targ.appendChild (scriptNode);
+}
