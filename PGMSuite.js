335e88eb540b866310ada9cb276d4b3dcf61cb61
Slimmmo <Slimmmo@users.noreply.github.com>
2017-01-21 12:21:50 +1100

    Update PGMSuite.js

--- PGMSuite.js
+++ PGMSuite.js
@@ -8,8 +8,8 @@
 // ==/UserScript==
 
 function indexOfPokemons(pokemon, pokemons) {
-    if ((false || !isPokemonChecked(pokemon.id)) &&
-       (pokemon.attack + pokemon.defence + pokemon.stamina) / 0.45 < 82) {
+    if ((document.getElementById('fcb').checked || !isPokemonChecked(pokemon.id)) &&
+       (pokemon.attack + pokemon.defence + pokemon.stamina) / 0.45 < document.getElementById('iiv').value) {
         return 0;
     } else {
         for (var i = 0; i < pokemons.length; ++i) {
@@ -19,14 +19,14 @@ function indexOfPokemons(pokemon, pokemons) {
             }
         }
     }
-    if (document.getElementById('cb').checked) {
-        postDiscord(pokemon);
+    if (document.getElementById('icb').checked) {
+       postDiscord(pokemon);
     }
     return -1;
 }
 
 function postDiscord(p) {
-    var webhookURL = ' INSERT DISCORD WEBHOOK HERE ';
+    var webhookURL = document.getElementById('iwh').value;
     var date = new Date(p.despawn * 1000);
     var seconds = "0" + date.getSeconds();
     var dateString = date.getHours() + ':' + ("0" + date.getMinutes()).substr(-2) + ':' + ("0" + date.getSeconds()).substr(-2);
@@ -42,19 +42,63 @@ function postDiscord(p) {
     });
 }
 
-var cb = document.createElement('input');
-cb.type = 'checkbox';
-cb.id = 'cb';
-var lab = document.createElement('label');
-lab.htmlFor = 'cb';
-lab.appendChild(document.createTextNode('Enable Discord'));
+function saveWebhook() {
+    localStorage.setItem('whURL', document.getElementById('iwh').value);
+}
+
+function modifyHTML() {
+    var icb = document.createElement('input');
+    icb.type = 'checkbox';
+    icb.id = 'icb';
+    var clab = document.createElement('label');
+    clab.htmlFor = 'icb';
+    clab.appendChild(document.createTextNode('Enable Discord'));
+    clab.style.cssText = 'padding-right: 7px';
+    document.getElementById('topbar').appendChild(icb);
+    document.getElementById('topbar').appendChild(clab);
+    
+    var iiv = document.createElement('input');
+    iiv.type = 'number';
+    iiv.id = 'iiv';
+    iiv.max = 100;
+    iiv.value = 82;
+    iiv.style.cssText = 'max-width: 45px';
+    var nlab = document.createElement('label');
+    nlab.htmlFor = 'iiv';
+    nlab.appendChild(document.createTextNode('IV %'));
+    nlab.style.cssText = 'padding-right: 7px';
+    document.getElementById('topbar').appendChild(iiv);
+    document.getElementById('topbar').appendChild(nlab);
+    
+    var fcb = document.createElement('input');
+    fcb.type = 'checkbox';
+    fcb.id = 'fcb';
+    var flab = document.createElement('label');
+    flab.htmlFor = 'fcb';
+    flab.appendChild(document.createTextNode('IV filter everything'));
+    flab.style.cssText = 'padding-right: 7px';
+    document.getElementById('topbar').appendChild(fcb);
+    document.getElementById('topbar').appendChild(flab);
+    
+    var iwh = document.createElement('input');
+    iwh.type = 'text';
+    iwh.id = 'iwh';
+    iwh.onchange = saveWebhook;
+    iwh.value = localStorage.getItem('whURL') || '';
+    iwh.style.cssText = 'max-width: 50px';
+    var wlab = document.createElement('label');
+    wlab.htmlFor = 'iwh';
+    wlab.appendChild(document.createTextNode('Webhook URL'));
+    document.getElementById('topbar').appendChild(iwh);
+    document.getElementById('topbar').appendChild(wlab);
+};
 
 // Inject this code into the site's code
 
-document.getElementById('topbar').appendChild(cb);
-document.getElementById('topbar').appendChild(lab);
+modifyHTML();
 addJS_Node (indexOfPokemons);
 addJS_Node (postDiscord);
+addJS_Node (saveWebhook);
 
 function addJS_Node (text, s_URL, funcToRun, runOnLoad) {
     var D                                   = document;
