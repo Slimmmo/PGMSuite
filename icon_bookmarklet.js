b758614e8ce22dd8d27a71fb6a53dc67b0f0369e
Slimmmo <Slimmmo@users.noreply.github.com>
2017-06-26 15:21:57 +1000

    Create icon_bookmarklet.js

---
+++ icon_bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:if(localStorage.getItem("infiniteScrollEnabled")===null){for(var i=1;i<=251;i++){localStorage['icon'+i]='https://raw.githubusercontent.com/pokeicons/icons/master/'+i+'.png';}}
