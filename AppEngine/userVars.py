c7febdfb970b26934ed5d41af87c0738a08c3914
Slimmmo <danielksimpson@gmail.com>
2018-02-11 15:59:36 +1100

    fixed uservars spacing/tabbing

--- AppEngine/userVars.py
+++ AppEngine/userVars.py
@@ -6,9 +6,9 @@ RAID_AREA = {
 }
 RAIDS = {
 	4: [
-    248, # Tyranitar
-    306 # Aggron
-  ],
+		248, # Tyranitar
+		306 # Aggron
+	],
 	5: None
 }
 GYM_NAMES = {
@@ -30,20 +30,20 @@ POKEMON_AREA = [
 ]
 # A value of 'None' means every spawn will be sent to Discord
 # The idea was to to be able to set a number and it will only send if IV% > number, currently not implemented
-# Every 100% IV will be sent even if the pokemon is not in this list
+# Every 90+% IV will be sent even if the pokemon is not in this list
 POKEMON = {
-  # GEN 1
-  3: None, # Venusaur
-  6: None, # Charizard
-  9: None, # Blastoise
+	# GEN 1
+	3: None, # Venusaur
+	6: None, # Charizard
+	9: None, # Blastoise
 	26: None, # Raichu
 	59: None, # Arcanine
 	65: None, # Alakazam
 	68: None, # Machamp
-  76: None, # Golem
-  89: None, # Muk
-  94: None, # Gengar
-  112: None, # Rhydon
+	76: None, # Golem
+	89: None, # Muk
+	94: None, # Gengar
+	112: None, # Rhydon
 	113: None, # Chansey
 	130: None, # Gyarados
 	131: None, # Lapras
@@ -53,17 +53,17 @@ POKEMON = {
 	144: None, # Articuno
 	145: None, # Zapdos
 	146: None, # Moltres
-  148: None, # Dragonair
+	148: None, # Dragonair
 	149: None, # Dragonite
 	150: None, # Mewtwo
 	151: None, # Mew
-  # GEN 2
-  154: None, # Meganium
-  157: None, # Typhlosion
-  160: None, # Feraligatr
-  176: None, # Togetic
+	# GEN 2
+	154: None, # Meganium
+	157: None, # Typhlosion
+	160: None, # Feraligatr
+	176: None, # Togetic
 	179: None, # Mareep
-  180: None, # Flaaffy
+	180: None, # Flaaffy
 	181: None, # Ampharos
 	201: None, # Unown
 	232: None, # Donphan
@@ -77,48 +77,48 @@ POKEMON = {
 	249: None, # Lugia
 	250: None, # Ho-Oh
 	251: None, # Celebi
-  # GEN 3
-  254: None, # Sceptile
-  256: None, # Combusken
-  257: None, # Blaziken
-  260: None, # Swampert
-  267: None, # Beautifly
-  269: None, # Dustox
-  272: None, # Ludicolo
-  275: None, # Shiftry
-  281: None, # Kirlia
-  282: None, # Gardevoir
-  288: None, # Vigoroth
-  289: None, # Slaking
-  295: None, # Exploud
-  306: None, # Aggron
-  308: None, # Medicham
-  319: None, # Sharpedo
-  321: None, # Wailord
-  328: None, # Trapinch
-  329: None, # Vibrava
-  330: None, # Flygon
-  342: None, # Crawdaunt
-  346: None, # Cradily
-  348: None, # Armaldo
-  349: None, # Feebas
-  350: None, # Milotic
-  358: None, # Chimecho
-  365: None, # Walrein
-  371: None, # Bagon
-  372: None, # Shelgon
-  373: None, # Salamence
-  374: None, # Beldum
-  375: None, # Metang
-  376: None, # Metagross
-  377: None, # Regirock
-  378: None, # Regice
-  379: None, # Registeel
-  380: None, # Latias
-  381: None, # Latios
-  382: None, # Kyogre
-  383: None, # Groudon
-  384: None, # Rayquaza
-  385: None, # Jirachi
-  386: None # Deoxys
+	# GEN 3
+	254: None, # Sceptile
+	256: None, # Combusken
+	257: None, # Blaziken
+	260: None, # Swampert
+	267: None, # Beautifly
+	269: None, # Dustox
+	272: None, # Ludicolo
+	275: None, # Shiftry
+	281: None, # Kirlia
+	282: None, # Gardevoir
+	288: None, # Vigoroth
+	289: None, # Slaking
+	295: None, # Exploud
+	306: None, # Aggron
+	308: None, # Medicham
+	319: None, # Sharpedo
+	321: None, # Wailord
+	328: None, # Trapinch
+	329: None, # Vibrava
+	330: None, # Flygon
+	342: None, # Crawdaunt
+	346: None, # Cradily
+	348: None, # Armaldo
+	349: None, # Feebas
+	350: None, # Milotic
+	358: None, # Chimecho
+	365: None, # Walrein
+	371: None, # Bagon
+	372: None, # Shelgon
+	373: None, # Salamence
+	374: None, # Beldum
+	375: None, # Metang
+	376: None, # Metagross
+	377: None, # Regirock
+	378: None, # Regice
+	379: None, # Registeel
+	380: None, # Latias
+	381: None, # Latios
+	382: None, # Kyogre
+	383: None, # Groudon
+	384: None, # Rayquaza
+	385: None, # Jirachi
+	386: None # Deoxys
 }
